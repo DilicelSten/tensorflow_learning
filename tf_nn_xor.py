034905c16bf355e3ba11718b75312ed3485ac498
DilicelSten <m13694245742@163.com>
2018-07-26 14:13:22 +0800

    neural network

---
+++ tf_nn_xor.py
@@ -0,0 +1,75 @@
+"""
+created on:2018/7/25
+author:DilicelSten
+target:Learn how to use tensowflow(NN for XOR)
+finished on:2018/7/25
+"""
+import tensorflow as tf
+import numpy as np
+
+x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
+y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+# params
+# NN
+# =====================================================
+W1 = tf.Variable(tf.random_normal([2, 2]), name="weight1")
+b1 = tf.Variable(tf.random_normal([2]), name="bias1")
+layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+
+W2 = tf.Variable(tf.random_normal([2, 1]), name="weight2")
+b2 = tf.Variable(tf.random_normal([1]), name="bias2")
+hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+
+# Wide NN
+# =======================================================
+# W1 = tf.Variable(tf.random_normal([2, 10]), name='weight1')
+# b1 = tf.Variable(tf.random_normal([10]), name='bias1')
+# layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+#
+# W2 = tf.Variable(tf.random_normal([10, 1]), name='weight2')
+# b2 = tf.Variable(tf.random_normal([1]), name='bias2')
+# hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+
+# Deep NN
+# =======================================================
+# W1 = tf.Variable(tf.random_normal([2, 10]), name='weight1')
+# b1 = tf.Variable(tf.random_normal([10]), name='bias1')
+# layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+#
+# W2 = tf.Variable(tf.random_normal([10, 10]), name='weight2')
+# b2 = tf.Variable(tf.random_normal([10]), name='bias2')
+# layer2 = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+#
+# W3 = tf.Variable(tf.random_normal([10, 10]), name='weight3')
+# b3 = tf.Variable(tf.random_normal([10]), name='bias3')
+# layer3 = tf.sigmoid(tf.matmul(layer2, W3) + b3)
+#
+# W4 = tf.Variable(tf.random_normal([10, 1]), name='weight4')
+# b4 = tf.Variable(tf.random_normal([1]), name='bias4')
+# hypothesis = tf.sigmoid(tf.matmul(layer3, W4) + b4)
+
+# cost function
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+# accuracy computation
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+# launch graph
+with tf.Session() as sess:
+    # initial tensorflow variables
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        sess.run(train, feed_dict={X: x_data, Y: y_data})
+        if step % 100 == 0:
+            print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run([W1, W2]))
+
+    # accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data})
+    print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
